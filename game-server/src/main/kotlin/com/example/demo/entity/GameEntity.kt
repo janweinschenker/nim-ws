93eb935fbddccdae1043ee9e226f0e89a9fc8f78
Jan Weinschenker <jan.weinschenker@holisticon.de>
2018-10-09 20:28:40 +0200

    feat: prepare task-02

--- game-server/src/main/kotlin/com/example/demo/entity/GameEntity.kt
+++ game-server/src/main/kotlin/com/example/demo/entity/GameEntity.kt
@@ -1,24 +1,10 @@
 package com.example.demo.entity
 
-import com.example.demo.common.model.Players
-import java.util.*
 import javax.persistence.Entity
-import javax.persistence.Id
-import javax.persistence.Table
 
 /**
  * The entity representing a nim game.
  */
 @Entity
-@Table(name = "game")
-class GameEntity(@Id
-                 var id: Long = Random().nextLong(),
-                 var initialItems: Int,
-                 var remainingItems: Int = initialItems,
-                 var finished: Boolean = false,
-                 var nextPlayer: Players = Players.HUMAN,
-                 var winner: Players? = null) {
 
-    // Empty constructor needed for mapstruct.
-    constructor() : this(initialItems = 13)
-}
+class GameEntity
