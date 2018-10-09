0975ad9eb4fd44c541086a2a640feb46b3a4b65f
Jan Weinschenker <jan.weinschenker@holisticon.de>
2018-10-09 21:01:43 +0200

    feat: add task 4

---
+++ game-player/src/main/kotlin/com/example/demo/player/game/Player.kt
@@ -0,0 +1,14 @@
+package com.example.demo.player.game
+
+import com.example.demo.common.model.Players
+import com.example.demo.common.model.TurnNotification
+import com.example.demo.player.restclient.Client
+import mu.KLogging
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
+
+@Service
+class Player(v) {
+
+
+}
