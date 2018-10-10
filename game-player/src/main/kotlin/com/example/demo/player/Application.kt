0ee29737bbb284a05fac7a4d638c12323bffd8c4
Jan Weinschenker <jan.weinschenker@holisticon.de>
2018-10-10 14:01:19 +0200

    fix: remove compile-errors

--- game-player/src/main/kotlin/com/example/demo/player/Application.kt
+++ game-player/src/main/kotlin/com/example/demo/player/Application.kt
@@ -1,20 +1,7 @@
 package com.example.demo.player
 
-import com.example.demo.player.game.Player
-import com.example.demo.player.restclient.Client
-import com.example.demo.player.websocket.MessageSessionHandler
-import com.example.demo.player.websocket.TurnSessionHandler
-import mu.KLogging
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.messaging.converter.MappingJackson2MessageConverter
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler
-import org.springframework.web.socket.client.standard.StandardWebSocketClient
-import org.springframework.web.socket.messaging.WebSocketStompClient
 
 
 @SpringBootApplication
@@ -23,3 +10,4 @@ open class Application
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
+}
--- game-player/src/main/kotlin/com/example/demo/player/game/Player.kt
+++ game-player/src/main/kotlin/com/example/demo/player/game/Player.kt
@@ -1,14 +1,9 @@
 package com.example.demo.player.game
 
-import com.example.demo.common.model.Players
-import com.example.demo.common.model.TurnNotification
-import com.example.demo.player.restclient.Client
-import mu.KLogging
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class Player(v) {
+class Player {
 
 
 }
