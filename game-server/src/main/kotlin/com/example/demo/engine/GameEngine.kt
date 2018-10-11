ce67fdf4b4f97fc58793c985c595e93129ee4960
Simon Sprünker <simon.spruenker@holisticon.de>
2018-10-11 18:16:40 +0200

    Added stub

--- game-server/src/main/kotlin/com/example/demo/engine/GameEngine.kt
+++ game-server/src/main/kotlin/com/example/demo/engine/GameEngine.kt
@@ -3,6 +3,15 @@ package com.example.demo.engine
 
 class GameEngine {
 
+    // fun play(game: GameEntity, numberOfItems: Int, player: Players): Pair<GameEntity, ValidationResult> {
+    // FIXME: Implement
+    //}
+
+    //@VisibleForTesting
+    //internal fun validateTurn(game: GameEntity, itemsToBeTaken: Int, playerMakingAMove: Players): ValidationResult {
+    // FIXME: Implement
+    //}
 
 }
 
+// FIXME Write data class for ValidationResult
