09aae3d5a2996d2021d0d0888423b3f3470c7fb4
Simon Sprünker <simon.spruenker@holisticon.de>
2018-10-19 16:49:18 +0200

    Fixed Readme, more comments in GameEngine

--- game-server/src/main/kotlin/com/example/demo/engine/GameEngine.kt
+++ game-server/src/main/kotlin/com/example/demo/engine/GameEngine.kt
@@ -1,8 +1,8 @@
 package com.example.demo.engine
 
+import com.example.demo.entity.GameEntity
 import com.example.demo.entity.Players
 import com.example.demo.entity.otherPlayer
-import com.example.demo.entity.GameEntity
 import com.google.common.annotations.VisibleForTesting
 import mu.KLogging
 import org.springframework.stereotype.Service
@@ -13,6 +13,9 @@ class GameEngine {
 
     companion object : KLogging()
 
+    /**
+     * Validates if the itemsToBeTaken can be taken by the playerMakingAMove on the given game.
+     */
     @VisibleForTesting
     internal fun validateTurn(game: GameEntity, itemsToBeTaken: Int, playerMakingAMove: Players): ValidationResult {
         logger.info { "Validating turn in ${game.id} for $playerMakingAMove" }
@@ -27,6 +30,11 @@ class GameEngine {
         return validationResult
     }
 
+    /**
+     * Executes the turn as defined by the given numberOfItems and the given player on the given game, if the turn is valid.
+     *
+     * Returns a validationResult and the new game state.
+     */
     fun play(game: GameEntity, numberOfItems: Int, player: Players): Pair<GameEntity, ValidationResult> {
         logger.info { "$player wants to take $numberOfItems items in ${game.id}." }
         val validationResult = validateTurn(game, numberOfItems, player)
